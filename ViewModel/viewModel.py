0de62373b97191dc421dbe8c5033afbf6b7a5772
Amacker Philipp <info@amackerautomation.ch>
2026-05-27 09:00:55 +0200

    not working Example

--- ViewModel/viewModel.py
+++ ViewModel/viewModel.py
@@ -4,12 +4,25 @@ import os
 cwd = os.getcwd()
 filename = cwd+"\\View\\data\\Base.stl"
 reader = pv.get_reader(filename)
-mesh = reader.read()
+BaseMesh = reader.read()
 
 filename = cwd+"\\View\\data\\InnerArm.stl"
 reader = pv.get_reader(filename)
 InnerArmMesh = reader.read()
 
-totalMesh = pv.merge([mesh,InnerArmMesh])
-totalMesh.plot()
+pl = pv.Plotter()
+base = pl.add_mesh(BaseMesh)
+actor = pl.add_mesh(InnerArmMesh)
 
+
+def callback(step):
+    base.position = [step, step, 0]
+    print(actor.position)
+
+
+pl.add_timer_event(max_steps=5000, duration=200, callback=callback)
+
+cpos = pv.CameraPosition(
+    position=(0.0, -1500.0, 850.0), focal_point=(0.0, 0.0, 0.0), viewup=(0.0, 1.0, 0.0)
+)
+pl.show(cpos=cpos)
