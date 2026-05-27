ef24522fa3113d6aa9650d39df2138be0e56e55f
Amacker Philipp <info@amackerautomation.ch>
2026-05-27 08:39:36 +0200

    Example For Pyvista

--- ViewModel/viewModel.py
+++ ViewModel/viewModel.py
@@ -1,17 +1,15 @@
 import pyvista as pv
-from pyvista import examples
+import os
 
-# Kitchen CFD simulation: grid with a velocity field
-# and furniture meshes as a separate dataset
-mesh = examples.download_kitchen()
-furniture = examples.download_kitchen(split=True)
+cwd = os.getcwd()
+filename = cwd+"\\View\\data\\Base.stl"
+reader = pv.get_reader(filename)
+mesh = reader.read()
 
-# Trace streamlines from above the stove
-source = pv.Line((0.08, 2.5, 0.71), (0.08, 4.5, 0.71), resolution=15)
-streamlines = mesh.streamlines_from_source(source, max_length=200)
+filename = cwd+"\\View\\data\\InnerArm.stl"
+reader = pv.get_reader(filename)
+InnerArmMesh = reader.read()
 
-# Compose the scene
-pl = pv.Plotter()
-pl.add_mesh(furniture, color='slategray')
-pl.add_mesh(streamlines.tube(radius=0.02))
-pl.show()
+totalMesh = pv.merge([mesh,InnerArmMesh])
+totalMesh.plot()
+
