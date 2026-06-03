a7469a142dc56cede4c6f3cb846665867b22fe9a
Amacker Philipp <info@amackerautomation.ch>
2026-06-03 11:16:36 +0200

    Threading

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import threading
 import Model.Scara as Scara
 import Model.CoreXY as CNC
 
 import View.Scara as ScaraView
 import View.CoreXY as CncView
 import ViewModel.HMI as Hmi
+
+
+def startHmi1():
+    hmiRobot1 = Hmi()
+def startHmi2():
+    hmiRobot2 = Hmi()
+def startHmi3():
+    hmiCnc = Hmi()
 
 
 if __name__ == "__main__":
@@ -13,6 +22,12 @@ if __name__ == "__main__":
 
     scaraView = ScaraView()
     cncView = CncView()
-    hmiRobot1 = Hmi()
-    hmiRobot2 = Hmi()
-    hmiCnc = Hmi()
+
+    hmiRobot1Thread = threading.Thread(target=startHmi1, args=("A", 3))
+    hmiRobot1Thread.start()
+
+    hmiRobot2Thread = threading.Thread(target=startHmi1, args=("A", 3))
+    hmiRobot2Thread.start()
+
+    hmiCncThread = threading.Thread(target=startHmi3, args=("A", 3))
+    hmiCncThread.start()   
